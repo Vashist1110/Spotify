056fb1a3f10443398011e321076864b115bf11aa
Vashist1110 <vashisttiwari2000@gmail.com>
2023-06-27 00:43:08 +0530

    added new songs

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Cheques", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Baller", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "No Love", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Still Rollin", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "We Rollin", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Tere Hawale-[Arijit Singh]", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Elevated", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Amplifier", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Safar", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
     {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
 ]
 
